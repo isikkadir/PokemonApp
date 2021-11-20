20ca6af11a33017ba93f47d3363510e61c478d8e
Kadir <78980888+isikkadir@users.noreply.github.com>
2021-11-20 20:55:59 +0300

    Initial commit

---
+++ app/src/main/java/com/isikkadir/pokemonapp/view/MainPage.kt
@@ -0,0 +1,9 @@
+package com.isikkadir.pokemonapp.view
+
+import androidx.fragment.app.Fragment
+import com.isikkadir.pokemonapp.R
+
+class MainPage : Fragment(R.layout.fragment_main_page) {
+
+
+}
---
+++ app/src/main/java/com/isikkadir/pokemonapp/view/PokemonDetails.kt
@@ -0,0 +1,8 @@
+package com.isikkadir.pokemonapp.view
+
+import androidx.fragment.app.Fragment
+import com.isikkadir.pokemonapp.R
+
+class PokemonDetails : Fragment(R.layout.fragment_pokemon_details) {
+
+}
