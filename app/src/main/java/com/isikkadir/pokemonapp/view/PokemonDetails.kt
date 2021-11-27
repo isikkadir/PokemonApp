5e4114c3c67d83e753bd66fa2975c40336b9f506
Kadir <78980888+isikkadir@users.noreply.github.com>
2021-11-28 02:34:24 +0300

    DetailsPage binding

--- app/src/main/java/com/isikkadir/pokemonapp/view/PokemonDetails.kt
+++ app/src/main/java/com/isikkadir/pokemonapp/view/PokemonDetails.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.RequestManager
 import com.isikkadir.pokemonapp.R
 import com.isikkadir.pokemonapp.databinding.FragmentMainPageBinding
 import com.isikkadir.pokemonapp.databinding.FragmentPokemonDetailsBinding
@@ -13,7 +14,9 @@ import com.isikkadir.pokemonapp.viewmodel.DetailsPageViewModel
 import com.isikkadir.pokemonapp.viewmodel.MainPageViewModel
 import javax.inject.Inject
 
-class PokemonDetails : Fragment(R.layout.fragment_pokemon_details) {
+class PokemonDetails @Inject constructor(
+    private val glide: RequestManager,
+) : Fragment(R.layout.fragment_pokemon_details) {
     private var binding: FragmentPokemonDetailsBinding? = null
     lateinit var viewModel: DetailsPageViewModel
 
@@ -40,8 +43,28 @@ class PokemonDetails : Fragment(R.layout.fragment_pokemon_details) {
 
         viewModel.pokemonPublic.observe(viewLifecycleOwner, {
             binding!!.detailsNumber.text = pokeNumber.toString()
+            binding!!.detailsName.text = it.name
+            if (it.abilities[0] != null) {
+                binding!!.firstAbility.text = it.abilities[0].ability.name
+            }
+            if (it.abilities[1] != null) {
+                binding!!.secondAbility.text = it.abilities[1].ability.name
+            }
+            binding!!.weightText.text = it.weight.toString()
+            binding!!.heightText.text = it.height.toString()
+            binding!!.progressHp.progress = it.stats[0].baseStat
+            binding!!.progressAtk.progress = it.stats[1].baseStat
+            binding!!.progressDef.progress = it.stats[2].baseStat
+            binding!!.progressSpAtk.progress = it.stats[3].baseStat
+            binding!!.progressSpDef.progress = it.stats[4].baseStat
+            binding!!.progressSpd.progress = it.stats[5].baseStat
+            binding!!.detailsImageView.background = null
+            val url =
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeNumber}.png"
+            glide.load(url).into(binding!!.detailsImageView);
         })
     }
+
 
     private fun getPokemonDetails(pokeNumber: Int) {
         viewModel.loadPokemonDetails(pokeNumber)
--- app/src/main/java/com/isikkadir/pokemonapp/view/PokemonFragmentFactory.kt
+++ app/src/main/java/com/isikkadir/pokemonapp/view/PokemonFragmentFactory.kt
@@ -2,17 +2,20 @@ package com.isikkadir.pokemonapp.view
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
+import com.bumptech.glide.RequestManager
 import com.isikkadir.pokemonapp.adapter.PokemonListAdapter
 import javax.inject.Inject
 
 class PokemonFragmentFactory @Inject constructor(
     private val adapter: PokemonListAdapter,
+    private val glide: RequestManager,
     ) : FragmentFactory() {
 
 
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
         return when (className) {
             MainPage::class.java.name -> MainPage(adapter)
+            PokemonDetails::class.java.name -> PokemonDetails(glide)
             else -> super.instantiate(classLoader, className)
         }
     }
